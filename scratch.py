6d4cc56f9e1b94511c0c127b5331a3b772523650
Anna <annatqwang@gmail.com>
2020-08-29 23:10:16 -0700

    added text detection for address and total cost

--- scratch.py
+++ scratch.py
@@ -3,19 +3,34 @@ from pdfminer.high_level import extract_text
 # this extracts the text from ____.pdf, which will be used to fill in the fields required on reimbursement forms
 text = extract_text('testfile2.pdf')
 
-# order receipts from digikey follow a rigid format. This dictionary defines the keywords preceding data we want
-# eg. name.
-kDict = {'findName': 'Fax 218-681-3380',
-         'country': 'Canada',
-         'findTotal': 'GST on taxable amount:'}
+# order receipts from digikey follow a rigid format. This dictionary defines the keywords to locate data we want.
+# text.find can then be used to easily find the starting index and end index of the string.
+kDict = {'findName': 'Fax 218-681-3380\n',
+         'findCountry': 'CANADA',
+         'findCost': 'GST on taxable amount: '}
 
-# this is the starting index of the data we want. It finds the starting index of the keyphrase,
-# then adds the length of the keyphrase. +2 to account for the line break
-nameStart = text.find(kDict.get('findName')) + len(kDict.get('findName')) + 2
+
+# this updates the google sheet on which the form is located
+def updatesheet(start, end):
+    print(text[start:end])
+    return 0
+
+
+# this is the starting index of the billing name. It finds the starting index of the keyphrase,
+# then adds the length of the keyphrase.
+nameStart = text.find(kDict.get('findName')) + len(kDict.get('findName'))
 nameEnd = text.find('\n', nameStart)
-print(text[nameStart:nameEnd])
+updatesheet(nameStart, nameEnd)
 
+# the billing address immediately follows the addressee's name.
+addressStart = nameEnd + 1
+addressEnd = text.find(kDict.get('findCountry')) + len(kDict.get('findCountry'))
+updatesheet(addressStart, addressEnd)
 
+costStart = text.find(kDict.get('findCost')) + len(kDict.get('findCost'))
+costEnd = text.find(' ', costStart)
+costTotal = float(text[costStart:costEnd])*1.05 +float(text[costStart:costEnd])*0.07
+print(costTotal)
 
 text_file = open("sample2.txt", "w")
 n = text_file.write(text)

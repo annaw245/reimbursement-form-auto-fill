d6dd6fc7f5fb1f47d1d775c96b81570a1c61a61c
Anna W <annatqwang@gmail.com>
2020-09-05 01:34:21 -0700

    prior to revert

--- scratch.py
+++ scratch.py
@@ -1,10 +1,17 @@
+# As treasurer of UBC Thunderbots, submitting reimbursement requests is an ongoing task.
+# After processing them manually, I observed receipts from digikey follow a strict layout.
+# Digikey is the distributor from which the electrical subteam sources their parts, accounting for 70% of the purchases.
+# This is now my sixth month, and I will streamline and semi-automate this process.
+
 from pdfminer.high_level import extract_text
+import gspread
 
 # this extracts the text from ____.pdf, which will be used to fill in the fields required on reimbursement forms
-text = extract_text('testfile2.pdf')
+text = extract_text('testfile.pdf')
 
-# order receipts from digikey follow a rigid format. This dictionary defines the keywords to locate data we want.
-# text.find can then be used to easily find the starting index and end index of the string.
+# As stated above, order receipts from digikey follow a rigid format.
+# This dictionary defines the keywords to locate data we want.
+# text.find can then be used to find the starting index and end index of the string.
 kDict = {'findName': 'Fax 218-681-3380\n',
          'findCountry': 'CANADA',
          'findCost': 'GST on taxable amount: '}
@@ -29,9 +36,9 @@ updatesheet(addressStart, addressEnd)
 
 costStart = text.find(kDict.get('findCost')) + len(kDict.get('findCost'))
 costEnd = text.find(' ', costStart)
-costTotal = float(text[costStart:costEnd])*1.05 +float(text[costStart:costEnd])*0.07
+costTotal = float(text[costStart:costEnd]) * 1.05 + float(text[costStart:costEnd]) * 0.07
 print(costTotal)
 
-text_file = open("sample2.txt", "w")
+text_file = open("sample1.txt", "w")
 n = text_file.write(text)
 text_file.close()

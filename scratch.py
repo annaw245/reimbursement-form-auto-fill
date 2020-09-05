6dd6c7a691e974d0293efd18f577b302fc99cffe
Anna <annatqwang@gmail.com>
2020-09-05 01:55:59 -0700

    restoring last repo

--- scratch.py
+++ scratch.py
@@ -9,7 +9,6 @@ import gspread
 # this extracts the text from ____.pdf, which will be used to fill in the fields required on reimbursement forms
 text = extract_text('testfile.pdf')
 
-<<<<<<< HEAD
 # As stated above, order receipts from digikey follow a rigid format.
 # This dictionary defines the keywords to locate data we want.
 # text.find can then be used to find the starting index and end index of the string.
@@ -27,28 +26,18 @@ def updatesheet(start, end):
 # this is the starting index of the billing name. It finds the starting index of the keyphrase,
 # then adds the length of the keyphrase.
 nameStart = text.find(kDict.get('findName')) + len(kDict.get('findName'))
-=======
-# order receipts from digikey follow a rigid format. This dictionary defines the keywords preceding data we want
-# eg. name.
-kDict = {'findName': 'Fax 218-681-3380',
-         'country': 'Canada',
-         'findTotal': 'GST on taxable amount:'}
-
-# this is the starting index of the data we want. It finds the starting index of the keyphrase,
-# then adds the length of the keyphrase. +2 to account for the line break
-nameStart = text.find(kDict.get('findName')) + len(kDict.get('findName')) + 2
->>>>>>> parent of 6d4cc56... added text detection for address and total cost
 nameEnd = text.find('\n', nameStart)
-print(text[nameStart:nameEnd])
+updatesheet(nameStart, nameEnd)
 
+# the billing address immediately follows the addressee's name.
+addressStart = nameEnd + 1
+addressEnd = text.find(kDict.get('findCountry')) + len(kDict.get('findCountry'))
+updatesheet(addressStart, addressEnd)
 
-<<<<<<< HEAD
 costStart = text.find(kDict.get('findCost')) + len(kDict.get('findCost'))
 costEnd = text.find(' ', costStart)
 costTotal = float(text[costStart:costEnd]) * 1.05 + float(text[costStart:costEnd]) * 0.07
 print(costTotal)
-=======
->>>>>>> parent of 6d4cc56... added text detection for address and total cost
 
 text_file = open("sample1.txt", "w")
 n = text_file.write(text)

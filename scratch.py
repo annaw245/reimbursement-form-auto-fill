57a89fff9f708e5284c78e6fe14f463a15dcfa47
Anna <annatqwang@gmail.com>
2020-09-05 14:55:50 -0700

    combined gspread access, redid credentials and arranged data into array

--- scratch.py
+++ scratch.py
@@ -5,6 +5,12 @@
 
 from pdfminer.high_level import extract_text
 import gspread
+import numpy as np
+
+# opening the sheet using
+gc = gspread.service_account()
+sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1W9OYU-Pw_moKfpbV9G4PgoLLqx1474criKSRsdx_w58/edit#gid=0')
+worksheet = sh.sheet1
 
 # this extracts the text from ____.pdf, which will be used to fill in the fields required on reimbursement forms
 text = extract_text('testfile.pdf')
@@ -16,10 +22,22 @@ kDict = {'findName': 'Fax 218-681-3380\n',
          'findCountry': 'CANADA',
          'findCost': 'GST on taxable amount: '}
 
+# this is the data we want to write to row. in order: Name, Date, Grand Total.
+myArr = []
+
 
 # this updates the google sheet on which the form is located
-def updatesheet(start, end):
-    print(text[start:end])
+def updateArray(start, end):
+    myArr.append(text[start:end])
+
+
+
+def updateLedger(array):
+    # currently, all empty rows in the sheet have a astrix placeholder (*). This is because gspread lacks the functionality
+    # to insert new rows. Hence we can use the worksheet.find function to determine the row number of the next empty row.
+    row = worksheet.find("*").row
+    col = 1
+    worksheet.update_cell(row, col, array[col])
     return 0
 
 
@@ -27,17 +45,17 @@ def updatesheet(start, end):
 # then adds the length of the keyphrase.
 nameStart = text.find(kDict.get('findName')) + len(kDict.get('findName'))
 nameEnd = text.find('\n', nameStart)
-updatesheet(nameStart, nameEnd)
+updateArray(nameStart, nameEnd)
 
 # the billing address immediately follows the addressee's name.
 addressStart = nameEnd + 1
 addressEnd = text.find(kDict.get('findCountry')) + len(kDict.get('findCountry'))
-updatesheet(addressStart, addressEnd)
+updateArray(addressStart, addressEnd)
 
 costStart = text.find(kDict.get('findCost')) + len(kDict.get('findCost'))
 costEnd = text.find(' ', costStart)
 costTotal = float(text[costStart:costEnd]) * 1.05 + float(text[costStart:costEnd]) * 0.07
-print(costTotal)
+myArr.append(costTotal)
 
 text_file = open("sample1.txt", "w")
 n = text_file.write(text)

c3953cadc7c9f31b01d1edd046d4fc3a7dccbcc5
Anna <annatqwang@gmail.com>
2020-09-12 23:32:07 -0700

    added: request user for file name using the command line

--- scratch.py
+++ scratch.py
@@ -1,8 +1,3 @@
-# As treasurer of UBC Thunderbots, submitting reimbursement requests is an ongoing task.
-# After processing them manually, I observed receipts from digikey follow a strict layout.
-# Digikey is the distributor from which the electrical subteam sources their parts, accounting for 70% of the purchases.
-# This is now my sixth month, and I will streamline and semi-automate this process.
-
 from pdfminer.high_level import extract_text
 import gspread
 
@@ -16,7 +11,11 @@ sh2 = gc.open_by_url(
 form = sh2.sheet1
 
 # this extracts the text from ____.pdf, which will be used to fill in the fields required on reimbursement forms
-text = extract_text('testfile.pdf')
+pdffile = input("Please enter the name of the file to be processed: ")
+if (".pdf" not in pdffile):
+    text = extract_text(pdffile + ".pdf")
+else:
+    text = extract_text(pdffile)
 
 kDict = {
     # As stated above, order receipts from digikey follow a rigid format.
